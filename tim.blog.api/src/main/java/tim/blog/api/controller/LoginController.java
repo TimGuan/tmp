151808ad04ad44eb1a249f9100815cbd71fbeeb9
TimGuan <gkqwt1990@gmail.com>
2014-01-27 15:13:15 +0800

    commit

--- tim.blog.api/src/main/java/tim/blog/api/controller/LoginController.java
+++ tim.blog.api/src/main/java/tim/blog/api/controller/LoginController.java
@@ -1,8 +1,8 @@
 package tim.blog.api.controller;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -15,20 +15,24 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequestMapping(value="login")
 public class LoginController {
+    /**
+     * api不包含登入，授权j_spring_security_check,j_spring_security_logout
+     * @param request
+     * @return
+     */
     @RequestMapping(value="dologin",method = RequestMethod.POST)
-    public String dologin(HttpServletRequest request,HttpServletResponse response){
-        if(request.getSession().getAttribute("username")==null||
-                ((String)request.getSession().getAttribute("username")).equals(""))
-        {
-            request.getSession().setAttribute("username",request.getParameter("username"));
-            System.out.println("111");
+    public String dologin(HttpServletRequest request){
+        HttpSession session=request.getSession(true);
+        if(!StringUtils.isEmpty(session.getAttribute("username"))){
+            return "have logined";
+        }else{
+            session.setAttribute("username",request.getParameter("username"));
+            return "success";
         }
-        return "success";
     }
     @RequestMapping(value="dologout")
-    public String dologin(HttpServletRequest request){
+    public String dologout(HttpServletRequest request){
         HttpSession session = request.getSession(false);
-        System.out.println(session.getAttribute("username"));
         if(session != null)
             session.invalidate();
         return "success";
